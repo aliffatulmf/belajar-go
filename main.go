40a3d37ee33e7be1f5097827a9a1749da5d1ea37
Alif Fatul <86809021+aliffatulmf@users.noreply.github.com>
2022-02-08 11:55:01 +0700

    add banner & variable

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func Banner(name string) {
+	var buff strings.Builder
+	for i := 0; i <= 10; i++ {
+		buff.WriteString("=")
+	}
+
+	fmt.Println(buff.String(), strings.ToTitle(name), buff.String())
+}
+
+func variable() {
+	Banner("variable")
+	var number int8
+	var text string
+	var char byte
+
+	number = 10
+	text = "Golang"
+	char = 'G'
+
+	fmt.Println("Number", number)
+	fmt.Println("Text", text)
+	fmt.Println("Character", char, "=>", string(char))
+
+	// multiple variable
+	var (
+		benar bool
+		salah string
+	)
+	benar = true
+	salah = "benarkan"
+
+	fmt.Println("Benar", benar)
+	fmt.Println("Salah", salah)
+
+	// simple
+	username := "aliffatul"
+
+	fmt.Println("username", username)
+}
 
 func main() {
 	fmt.Println("Let's GO!")
+
+	variable()
+
 }
